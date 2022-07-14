90af75284c314186a66bf44a8a54f7cbcd2b74e0
Tung Pham <phamduytung11@gmail.com>
2022-07-14 17:23:52 +0700

    update gate and policy

--- app/User.php
+++ app/User.php
@@ -38,4 +38,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles() {
+        return $this->belongsToMany(Role::class, 'role_user', 'user_id', 'role_id');
+    }
+
+    public function checkPermissionAccess($permissionCheck) {
+        $roles = auth()->user()->roles;
+        foreach ($roles as $role) {
+            $permissions = $role->permissions;
+            // kiểm tra xem biến truyền có được vào không
+            if ($permissions->contains('key_code', $permissionCheck)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
